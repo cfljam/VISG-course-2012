f42c1c5901d08f84bc88412757d41a8f57310851
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-17 23:10:05 +1300

    verbose

--- 16.Samtools_Filter_Pool2/run.sh
+++ 16.Samtools_Filter_Pool2/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "using samtools to filter  sam files"
-echo " run on all populations"
-for Pop in {2..8}; do  samtools view -q 20 -Sb ../15.bwa_sw_Pool2/Pool2_Pop$Pop.sam | samtools sort - Pool2_Pop$Pop.filtered ; done
+echo "creating a single filtered bam file"
+samtools view -q 20 -Sb ../15.bwa_sw_Pool2/Pool2_Pop2.sam | samtools sort -  Pool2_Pop2_filtered
+echo "now run on all populations"
+for N in {2..8}; do  samtools view -q 20 -Sb ../15.bwa_sw_Pool2/Pool2_Pop$N.sam | samtools sort - Pool2_Pop$N.filtered ; done
