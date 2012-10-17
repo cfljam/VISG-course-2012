867eb1f1e35d53e695a1474bb74a8492ed2a23fe
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-17 23:06:44 +1300

    typo

--- 11.Samtools_Filter_Pool1/run.sh
+++ 11.Samtools_Filter_Pool1/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "using samtools to filter  sam files"
-echo "creating a sinlgle filtered bam file"
+echo "creating a single filtered bam file"
  samtools view -q 20 -Sb ../10.bwa_sw_Pool1/Pool1_Pop2.sam | samtools sort -  Pool1_Pop2_filtered
 echo "now run on all populations"
  for N in {2..8}; do  samtools view -q 20 -Sb ../10.bwa_sw_Pool1/Pool1_Pop$N.sam | samtools sort - Pool1_Pop$N.filtered ; done
