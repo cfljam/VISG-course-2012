0be72ebd318d5cf60ada2694fe0362a0cd74a3b3
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-19 16:47:32 +1300

    shebang, and verbose fixes

--- 15.bwa_sw_Pool2/run.sh
+++ 15.bwa_sw_Pool2/run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-echo "first check our command line"
-for N in {2..8}; do echo "bwa bwasw -f Pool2_Pop$N.sam../05.reference/pool2.fasta ../00.raw/Pool2_BARCODE$N.fastq";done
-echo "now running for real"
-for N in {2..8}; do bwa bwasw -f Pool2_Pop$N.sam ../05.reference/pool2.fasta  ../00.raw/Pool2_BARCODE$N.fastq;done
 
-#create an unfiltered  bam file just to compare sizes
+## Constructing 'bwa bwasw' heuristic Smith-Waterman-like alignment command for long reads
+echo "first check our command line"
+for N in {2..8}
+do 
+  echo "bwa bwasw -f Pool2_Pop$N.sam../05.reference/pool2.fasta ../00.raw/Pool2_BARCODE$N.fastq"
+done
+
+## Running 'bwa bwasw' heuristic Smith-Waterman-like alignment for long reads
+echo "now running for real"
+for N in {2..8}
+  do bwa bwasw -f Pool2_Pop$N.sam ../05.reference/pool2.fasta  ../00.raw/Pool2_BARCODE$N.fastq
+done
+
+## Create an unfiltered  bam file just to compare sizes
 samtools view -bS -o Pool2_Pop2_unfiltered.bam Pool2_Pop2.sam
