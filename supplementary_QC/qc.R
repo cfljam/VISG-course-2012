fa3477a5247eb9b864238dc5316705cf9122784a
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-19 19:59:50 +1300

    sep globals

--- supplementary_QC/qc.R
+++ supplementary_QC/qc.R
@@ -1,53 +1,72 @@
 #!/usr/bin/env Rscript
 
+# Clear working directory objects
+rm(list=ls())
+
 require(ShortRead)
 
-## Globals
-basedir   <- "~/VISG-course-2012"
-path      <- file.path(basedir, "supplementary_QC")
-if(getwd() != path) {
-  setwd(path)
-}
+# Getting vignette help
+if(interactive()) vignette("Overview") ## Quality assessment, page 8
 
-indir     <- file.path(basedir, "00.raw")
-outdir    <- file.path(basedir, "01.qc")
-fileNames <- dir(indir, pattern="fastq$")
-suppressWarnings(dir.create(outdir))
 
-## Path to first file
+# Globals
+cat("[ Code to source() ]\n")
+cat(readLines("globals.R"), sep="\n")
+
+# source globals code
+source("globals.R")
+
+cat("[ Directory and Fastq filenames ]\n")
+print(indir)
+print(fileNames)
+
+
+## Path to first example Fastq file
 firstFile <- file.path(indir, fileNames[1])
+
 
 ## Read in Fastq example
 rfq       <- readFastq(firstFile, qualityType="Auto")
 
+
 ## Show method
 print(rfq)
+
 
 ## Class of rfq is an instance of ShortReadQ S4 class
 class(rfq)
 
+
 ## Print all S4 class slotNames
 slotNames(rfq)
+
 
 ## sread accessor method
 sread(rfq)
 
+
 ## id accessor method
 id(rfq)
+
 
 ## quality accessor method
 quality(rfq)
 
+
 ## Run Quality control script
 qaSummary <- qa(rfq, lane="Roche 454")
+
 
 ## Create hmtml report
 fname <- report(qaSummary, dest="/tmp")
 
+
 ## Open html report 
 if(interactive()) browseURL(fname)
 
-## Investigating qaSummary content (include??)
+
+## Snippet to Investigate qaSummary structure
+cat("[ qaSummary structure ]\n")
 for(i in names(qaSummary)) {
   if(i == "perTile") next;
   if(i == "perCycle") {
@@ -63,15 +82,16 @@ for(i in names(qaSummary)) {
   }
 }
 
+
 ## Generate qa reports for all Fastq files
 for(f in fileNames) {
-  ## Truncate the ".fastq$" off names
+  # Truncate the ".fastq$" off names
   fdir      <- gsub("\\..+?$", "", f)
-  ## Generate a qc object
+  # Generate a qc object
   qaSummary <- qa(indir, pattern=f, lane=fdir, type="fastq")
-  ## Generate an html report
-  fname     <- report(qaSummary, dest=file.path(outdir, fdir))
-  ## Be verbose
+  # Generate an html report
+  fname     <- report(qaSummary, dest=file.path(path, fdir))
+  # Be verbose
   cat("[", fdir , "=>", fname, "]\n")
 }
 
--- supplementary_QC/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-## Run qa.R script as a batch file
-Rscript qc.R 2> err > log 
