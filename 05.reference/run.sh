e6b5ac3b55a4cc33f1ecaceb322939b10aba5e0c
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-19 16:34:37 +1300

    modify shebang, and make loop readible

--- 05.reference/run.sh
+++ 05.reference/run.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-for ref in `ls *.fasta`; do  bwa index $ref; done
+#!/usr/bin/env bash
+
+## Flow control using a for loop to run 'bwa index'
+for ref in `ls *.fasta`; 
+do  
+  bwa index $ref
+done
