e8157d42e871e7ea9ca8c235934851b40cc15e73
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-19 16:52:30 +1300

    mpileup code

---
+++ 20.mpileup/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+##call variants with mpileup
+samtools mpileup  -d 10000 -f ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam > Pool1.mpileup
+samtools mpileup  -d 10000 -f ../05.reference/pool2.fasta ../16.Samtools_Filter_Pool2/*.bam > Pool2.mpileup
