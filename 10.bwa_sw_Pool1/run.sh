a8540c65810fb932fb36dbc0ae816ab27b7366f5
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-19 16:39:26 +1300

    shebang call...

--- 10.bwa_sw_Pool1/run.sh
+++ 10.bwa_sw_Pool1/run.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+## running 'bwa bwasw' heuristic Smith-Waterman-like alignment for long reads
+
 echo "first check our command line"
-for N in {2..8}; do echo "bwa bwasw -f Pool1_Pop$N.sam ../05.reference/pool1.fasta ../00.raw/Pool1_BARCODE$N.fastq";done
+for N in {2..8}; 
+do 
+  echo "bwa bwasw -f Pool1_Pop$N.sam ../05.reference/pool1.fasta ../00.raw/Pool1_BARCODE$N.fastq"
+done
+
 echo "now running for real"
-for N in {2..8}; do bwa bwasw -f Pool1_Pop$N.sam ../05.reference/pool1.fasta  ../00.raw/Pool1_BARCODE$N.fastq;done
+for N in {2..8}; 
+do 
+  bwa bwasw -f Pool1_Pop$N.sam ../05.reference/pool1.fasta  ../00.raw/Pool1_BARCODE$N.fastq
+done
 
 #create an unfiltered  bam file just to compare sizes
 samtools view -bS -o Pool1_Pop2_unfiltered.bam Pool1_Pop2.sam 
