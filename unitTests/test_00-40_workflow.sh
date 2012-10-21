3656542db73fd26eb99778e0f9ac5954967377e4
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-21 23:20:39 +1300

    traverse to directories

--- unitTests/test_00-40_workflow.sh
+++ unitTests/test_00-40_workflow.sh
@@ -4,28 +4,43 @@ dir=$HOME/VISG-course-2012
 
 ## Run each bash script
 echo "[ 05 unit test ]" >&2
-$dir/05.reference/run.sh
+cd $dir/05.reference/
+./run.sh
+cd -
 
 echo "[ 10 unit test ]" >&2
-$dir/10.bwa_sw_Pool1/run.sh
+cd $dir/10.bwa_sw_Pool1/
+./run.sh
+cd -
 
 echo "[ 11 unit test ]" >&2
-$dir/11.Samtools_Filter_Pool1/run.sh
+cd $dir/11.Samtools_Filter_Pool1
+./run.sh
+cd -
 
 echo "[ 15 unit test ]" >&2
-$dir/15.bwa_sw_Pool2/run.sh
+cd $dir/15.bwa_sw_Pool2
+./run.sh
+cd -
 
 echo "[ 16 unit test ]" >&2
-$dir/16.Samtools_Filter_Pool2/run.sh
+cd $dir/16.Samtools_Filter_Pool2
+./run.sh
+cd -
 
 echo "[ 20 unit test ]" >&2
-$dir/20.mpileup/run.sh
+cd $dir/20.mpileup
+./run.sh
 
 echo "[ 25 unit test ]" >&2
-$dir/25.bcftools/run.sh
+cd $dir/25.bcftools
+./run.sh
+cd -
 
 echo "[ 30 unit test ]" >&2
-$dir/30.popoolation/run.sh
+cd $dir/30.popoolation
+./run.sh
+cd -
 
 echo "[ 35 unit test ]" >&2
 cd $dir/35.Visualise_FET
@@ -33,4 +48,6 @@ Rscript FET_plots.R
 cd -
 
 echo "[ 40 unit test ]" >&2
-$dir/40.igv/run.sh
+cd $dir/40.igv/
+./run.sh
+cd -
