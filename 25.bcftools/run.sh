431c9367aacdecb1e46acd313cc56050aa40bb67
mdavy86 <mdavy86@gmail.com>
2012-10-23 21:53:32 +1300

    Update 25.bcftools/run.sh

--- 25.bcftools/run.sh
+++ 25.bcftools/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 echo "Calling snps in Pool1"
 samtools mpileup -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
