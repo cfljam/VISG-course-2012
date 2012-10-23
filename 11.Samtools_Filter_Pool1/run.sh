888ca67eb15bf6936d0e1ee145d67409c4308aab
visg_user <visg_user@cfljam-HP-EliteBook-8460p.(none)>
2012-10-24 11:35:56 +1300

    fix name

--- 11.Samtools_Filter_Pool1/run.sh
+++ 11.Samtools_Filter_Pool1/run.sh
@@ -3,7 +3,8 @@
 ## Create a sorted bam file filtered with minimum quality of 20 (MAPQ column 5 in sam file) 
 echo "using samtools to filter  sam files"
 echo "creating a single filtered bam file"
-samtools view -q 20 -Sb ../10.bwa_sw_Pool1/Pool1_Pop2.sam | samtools sort -  Pool1_Pop2_filtered
+
+samtools view -q 20 -Sb ../10.bwa_sw_Pool1/Pool1_Pop2.sam | samtools sort -  Pool1_Pop2.filtered
 
 ## Looping through all sam files
 echo "now run on all populations"
