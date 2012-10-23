6ed82a19cfd62c32579d3c963588af7357832652
davy86 <marcus.davy@plantandfood.co.nz>
2012-10-24 07:51:25 +1300

    added popoolation perl prerequisites

--- 25.bcftools/run.sh
+++ 25.bcftools/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 echo "Calling snps in Pool1"
-samtools mpileup -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
+samtools mpileup -d 10000 -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
+#samtools mpileup -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
 echo "Calling snps in Pool2"
-samtools mpileup -ugf ../05.reference/pool2.fasta ../16.Samtools_Filter_Pool2/*.bam  | bcftools view -ve - > Pool2.vcf
+#samtools mpileup -ugf ../05.reference/pool2.fasta ../16.Samtools_Filter_Pool2/*.bam  | bcftools view -ve - > Pool2.vcf
 
