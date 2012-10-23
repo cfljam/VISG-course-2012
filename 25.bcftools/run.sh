d07f2f3a89005a807bdbca6298e52c089d183877
visg_user <visg_user@cfljam-HP-EliteBook-8460p.(none)>
2012-10-24 11:18:31 +1300

    rv original code

--- 25.bcftools/run.sh
+++ 25.bcftools/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Calling snps in Pool1"
-samtools mpileup -d 10000 -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
-#samtools mpileup -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
+samtools mpileup -ugf ../05.reference/pool1.fasta ../11.Samtools_Filter_Pool1/*.bam  | bcftools view -ve - > Pool1.vcf
 echo "Calling snps in Pool2"
-#samtools mpileup -ugf ../05.reference/pool2.fasta ../16.Samtools_Filter_Pool2/*.bam  | bcftools view -ve - > Pool2.vcf
+samtools mpileup -ugf ../05.reference/pool2.fasta ../16.Samtools_Filter_Pool2/*.bam  | bcftools view -ve - > Pool2.vcf
 
