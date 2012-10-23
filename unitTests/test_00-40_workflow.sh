06df7fbf0b2ddab905f3fbf901ef93ec0abcfd6b
visg_user <visg_user@cfljam-HP-EliteBook-8460p.(none)>
2012-10-24 10:47:17 +1300

    times

--- unitTests/test_00-40_workflow.sh
+++ unitTests/test_00-40_workflow.sh
@@ -10,44 +10,44 @@ cd -
 
 echo "[ 10 unit test ]" >&2
 cd $dir/10.bwa_sw_Pool1/
-./run.sh
+time ./run.sh
 cd -
 
 echo "[ 11 unit test ]" >&2
 cd $dir/11.Samtools_Filter_Pool1
-./run.sh
+time ./run.sh
 cd -
 
 echo "[ 15 unit test ]" >&2
 cd $dir/15.bwa_sw_Pool2
-./run.sh
+time ./run.sh
 cd -
 
 echo "[ 16 unit test ]" >&2
 cd $dir/16.Samtools_Filter_Pool2
-./run.sh
+time ./run.sh
 cd -
 
 echo "[ 20 unit test ]" >&2
 cd $dir/20.mpileup
-./run.sh
+time ./run.sh
 
-echo "[ 25 unit test ]" >&2
-cd $dir/25.bcftools
-./run.sh
-cd -
+#echo "[ 25 unit test ]" >&2
+#cd $dir/25.bcftools
+# time ./run.sh
+#cd -
 
 echo "[ 30 unit test ]" >&2
 cd $dir/30.popoolation
-./run.sh
+time ./run.sh
 cd -
 
 echo "[ 35 unit test ]" >&2
 cd $dir/35.Visualise_FET
-Rscript FET_plots.R
+time Rscript FET_plots.R
 cd -
 
 echo "[ 40 unit test ]" >&2
 cd $dir/40.igv/
-./run.sh
+time ./run.sh
 cd -
